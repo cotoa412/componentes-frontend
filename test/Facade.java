544c74febbc2a1c482ad648f680d45bd2b397020
Dacord <dacord97@hotmail.com>
2019-11-10 08:45:56 -0600

    +added Register from and xhtml page +added Login form and xhtml page +css classes are reusable

---
+++ test/Facade.java
@@ -0,0 +1,38 @@
+
+import com.componentes.controlador.UsuarioController;
+import com.componentes.entidades.Usuario;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author DacordMachine
+ */
+public class Facade {
+    
+    public void login(String username, String password){
+        
+        Usuario u = new Usuario();
+        u.setNombre(username);
+        u.setConstrania(password);
+        UsuarioController uc = new UsuarioController();
+        
+        
+    }
+    
+    public void registrar(String nombre, String correo, String password){
+        
+        Usuario u = new Usuario();
+        u.setNombre(nombre);
+        u.setCorreo(correo);
+        u.setConstrania(password);
+        UsuarioController uc = new UsuarioController();
+        uc.Insert(u);
+        
+    }
+    
+}
